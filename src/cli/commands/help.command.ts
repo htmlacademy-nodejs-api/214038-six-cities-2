0b0e67f100ba6d71fedc3fdde6a6d8536c703c1e
eperkhalsky <egor.perkhalsky@amasty.com>
2025-07-11 14:20:31 +0300

    Install chalk

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import chalk from 'chalk';
 
 export class HelpCommand implements Command {
   public getName(): string {
@@ -6,15 +7,15 @@ export class HelpCommand implements Command {
   }
 
   public async execute(..._parameters: string[]): Promise<void> {
-    console.info(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
-            cli.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
-    `);
+    const log = console.log;
+
+    log(chalk.red('Программа для подготовки данных для REST API сервера.'));
+    log(chalk.blue('Пример:'));
+    log(chalk.green('    cli.js --<command> [--arguments]'));
+    log(chalk.blue('Команды:'));
+    log(chalk.green('    --version:                   # выводит номер версии'));
+    log(chalk.green('    --help:                      # печатает этот текст'));
+    log(chalk.green('    --import <path>:             # импортирует данные из TSV'));
+    log(chalk.green('    --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных'));
   }
 }
